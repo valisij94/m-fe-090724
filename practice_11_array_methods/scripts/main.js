461ae13ecce25b114eb395a20807fc20f26f78dc
Davy <davyd.dabahian@cheetahdigital.com>
2024-11-01 01:39:04 +0200

    practice 11 array methods

---
+++ practice_11_array_methods/scripts/main.js
@@ -0,0 +1,14 @@
+// Подопытные данные
+
+// Массив чисел (примитивы)
+const numbers = [1,3,6,2,4,0,9,7,5,10,15,12,11,9,2,7];
+
+// Массив строк (примитивы)
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+const filters = ['foo', 'bar', 'baz', 'of', 'is'];
+const search = ['timon', 'is', 'a', 'suricate']
+
+const simpleElems = ['this', 'is', 'that', 'or', 'this', 'is', 'are', 'yes', 'are'];
+
+const years = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"];
+const elements = ["Wood", "Fire", "Earth", "Metal", "Water"];
