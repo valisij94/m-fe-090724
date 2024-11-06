4d10d6b84ac8b6408b64b2007b414a70856e7c63
Davy <davyd.dabahian@cheetahdigital.com>
2024-11-06 22:54:31 +0200

    practice 13 sort reduce

---
+++ practice_13_sort_reduce/scripts/main.js
@@ -0,0 +1,9 @@
+// Подопытные данные
+
+// Массив чисел (примитивы)
+const numbers = [1,3,6,2,4,0,9,7,5,10,15,12,11,9,2,7];
+
+// Массив строк (примитивы)
+const strings = ['this', 'is', 'an', 'array', 'of', 'simple', 'strings'];
+
+const complexStrings = ['this', 'is', 'also', 'an', 'array', 'of', 'not', 'so', 'simple', 'strings'];
