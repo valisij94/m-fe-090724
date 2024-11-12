eaed97aa9cc93fc8fdc502ba8c616860309404c1
Davy <davyd.dabahian@cheetahdigital.com>
2024-11-12 12:50:59 +0200

    practice 14 DOM

---
+++ practice_14_DOM/scripts/main.js
@@ -0,0 +1,13 @@
+// Подопытные данные
+
+// Массив массивов
+const strangers = [
+  ['Gimli', 'Axe', 'Dwarwes', 139],
+  ['Legolas', 'Longbow', 'Elves', 501 ],
+  ['Aragorn', 'Sword', 'People', 87],
+  ['Boromir', 'Sword', 'People', 41],
+  ['Frodo', 'Sword', 'Hobbit', 50],
+  ['Sam', 'Sword', 'Hobbit', 38],
+  ['Meriadok', 'Sword', 'Hobbit', 36],
+  ['Peregrin', 'Sword', 'Hobbit', 28],
+];
