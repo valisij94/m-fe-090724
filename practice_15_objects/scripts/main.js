02ab2a5d1b59b2c3f95c9f7fcdea5ed6d3a8e0de
Davy <davyd.dabahian@cheetahdigital.com>
2024-11-13 22:44:36 +0200

    practice 15 objects

---
+++ practice_15_objects/scripts/main.js
@@ -0,0 +1,30 @@
+// Подопытные данные
+
+const simpleDwarf = {
+  name: 'Torin',
+  alias: 'The Oakshield',
+  age: 324,
+  weapons: ['sword', 'axe', 'bow']
+}
+
+const armoredDwarf = {
+  name: 'Dvalin',
+  age: 407,
+  weapons: ['axe', 'axe', 'hammer'],
+  armour: {
+    head: 'Steel Helmet',
+    body: 'Mitril Armour',
+    shield: 'Oaken shield'
+  }
+}
+
+const participants = [
+  { name: 'Aragorn', race: 'People', age: 87, weapons: ['Sword', 'Spear', 'Dagger'] },
+  { name: 'Gimli', race: 'Dwarwes', age: 139, weapons: ['Axe'] },
+  { name: 'Legolas', race: 'Elves', age: 501, weapons: ['Bow', 'Sword', 'Sword'] },
+  { name: 'Boromir', race: 'People', age: 41, weapons: ['Sword', 'Dagger'] },
+  { name: 'Frodo', race: 'Hobbits', age: 50, weapons: ['Dagger'] },
+  { name: 'Sam', race: 'Hobbits', age: 38, weapons: ['Sword'] },
+  { name: 'Meriadok', race: 'Hobbits', age: 36, weapons: ['Sword'] },
+  { name: 'Peregrin', race: 'Hobbits', age: 28, weapons: ['Sword'] },
+];
