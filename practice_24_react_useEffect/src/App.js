42a915c4766ab3cb3757daf5b481784c1516ca24
Davy <davyd.dabahian@cheetahdigital.com>
2024-12-17 15:42:11 +0200

    practice 24 results

--- practice_24_react_useEffect/src/App.js
+++ practice_24_react_useEffect/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SimpleList from './components/SimpleList';
 
@@ -19,8 +19,38 @@ function App() {
     }
   }
 
+  const [listShow, setListShow] = useState(true);
+
+  const showHideBtnClickHandler = () => {
+    setListShow( !listShow );
+  }
+
+  const [current, setCurrent] = useState(new Date().toLocaleString());
+
+  useEffect( () => {
+    const intervalId = setInterval( () => {
+      setCurrent(new Date().toLocaleString());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+
+  const [products, setProducts] = useState([]);
+
+  useEffect( () => {
+    fetch('https://dummyjson.com/products')
+      .then( resp => resp.json())
+      .then( data => {
+        console.log(data.products);
+        setProducts(data.products);
+      });
+  }, []);
+
   return (
     <div className="App">
+      <p>{current}</p>
       <input
         type='text'
         onChange={inputChangeHandler}
@@ -28,7 +58,11 @@ function App() {
         value={input}
       />
       <button onClick={addTodoHandler}>Add Todo</button>
-      <SimpleList  listHeaderText={"Simple List Header"} listSource={todos}/>
+
+      <button onClick={showHideBtnClickHandler}>{listShow ? 'Hide list' : 'Show list'}</button>
+      { listShow &&
+        <SimpleList  listHeaderText={"Simple List Header"} listSource={todos}/>
+      }
     </div>
   );
 }
--- practice_24_react_useEffect/src/components/SimpleList.jsx
+++ practice_24_react_useEffect/src/components/SimpleList.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+
 export default function SimpleList( {listHeaderText, listSource} ) {
+
+  useEffect( () => {
+    console.log('List is mounted!');
+
+    return () => {
+      console.log('List is unmounted!');
+    }
+  }, []);
+
+  useEffect( () => {
+    console.log('ListSource is changed!')
+  }, [listSource]);
 
   return (
     <>
