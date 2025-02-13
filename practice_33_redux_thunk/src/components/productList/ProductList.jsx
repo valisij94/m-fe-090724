4cd3b5c8ba268a18d29474f61814c078610982da
Davy <davyd.dabahian@cheetahdigital.com>
2025-02-13 13:21:37 +0200

    practice useRef

--- practice_33_redux_thunk/src/components/productList/ProductList.jsx
+++ practice_33_redux_thunk/src/components/productList/ProductList.jsx
@@ -1,40 +1,22 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import ProductCard from "../productCard/ProductCard";
 import { ThemeContext } from "../../context/themeContext/ThemeContext";
 import { useSelector, useDispatch } from 'react-redux';
-import { errorFetching, finishFetching, startFetching } from "../../store/slices/productsSlice";
+import { fetchProducts } from "../../store/slices/productsSlice";
 
 export default function ProductList( {selectedCategory} ) {
 
-  const products = useSelector( state => state.products.products);
+  const { products, error } = useSelector( state => state.products);
   const dispatch = useDispatch();
   const filters = useSelector( state => state.filter);
 
-  console.log('Render prods', filters)
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   useEffect( () => {
-    const requestProducts = async () => {
-      try {
-        dispatch( startFetching() );
-        let url = 'https://dummyjson.com/products';
-        const resp = await fetch(url);
-        if (!resp.ok) {
-          throw new Error('Failed to fetch! ' + resp.statusText);
-        }
-        const data = await resp.json();
-        dispatch( finishFetching(data.products) );
-      }
-      catch (err) {
-        dispatch( errorFetching(err.message || err) );
-      }
-    }
-
-    requestProducts();
+    dispatch( fetchProducts() );
   }, []);
 
   const getFilteredProducts = () => {
-    console.log('Call filter', filters)
     return products.filter( product => {
       let result = true;
       if (filters.category) {
@@ -57,9 +39,9 @@ export default function ProductList( {selectedCategory} ) {
     <div className={`productListContainer ${theme}`}>
       <h2>Products in the shop</h2>
       <button onClick={toggleTheme}>Toggle Theme from ProductList</button>
-      <div className="productList">
+      { !error ? <div className="productList">
         { getFilteredProducts().map( el => <ProductCard key={el.id} product={el}/> )}
-      </div>
+      </div> : <p>{error}</p> }
     </div>
   );
 }
--- practice_33_redux_thunk/src/store/slices/productsSlice.js
+++ practice_33_redux_thunk/src/store/slices/productsSlice.js
@@ -1,5 +1,14 @@
 // src/redux/slices/counterSlice.js
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+  const resp = await fetch('https://dummyjson.com/products');
+  if (!resp.ok) {
+    throw new Error('Failed to fetch! ' + resp.statusText);
+  }
+  const data = await resp.json();
+  return data;
+});
 
 export const productsSlice = createSlice({
   name: 'products',
@@ -8,23 +17,21 @@ export const productsSlice = createSlice({
     isFetching: false,
     error: ''
   },
-  reducers: {
-    startFetching: (state) => {
-      state.error = '';
-      state.isFetching = true;
-    },
-    finishFetching: (state, action) => {
-      state.isFetching = false;
-      state.products = action.payload;
-    },
-    errorFetching: (state, action) => {
-      state.isFetching = false;
-      state.error = action.payload;
-    }
-  },
-})
-
-// Action creators are generated for each case reducer function
-export const { startFetching, errorFetching, finishFetching } = productsSlice.actions
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.isFetching = true;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.isFetching = false;
+        state.products = action.payload.products;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.isFetching = false;
+        state.error = action.error.message;
+      })
+  }
+});
 
 export default productsSlice.reducer
